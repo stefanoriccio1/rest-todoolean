7eeec2d055a976b5d62eb1f2f5a1df43ea4d157c
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-13 15:58:52 +0100

    riuscito a fare delete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // alert('ciao');
 $(document).ready(function(){
+ deleteData(prompt('dammi ID da eliminare'));
+});
+function getData(){
   $.ajax({
     url: "http://157.230.17.132:3031/todos",
     method: "GET",
@@ -9,38 +12,37 @@ $(document).ready(function(){
       var toDo = data;
       for (var i = 0; i < toDo.length; i++) {
         console.log(toDo[i]);
+        toDo[0].push(getdata(prompt('dammi il tuo nome')))
       }
      },
     error: function (richiesta, stato, errori) {
        alert("E' avvenuto un errore. " + errore); }
     });
+}
 
-    $.ajax({
-      url: "http://157.230.17.132:3031/todos",
-      method: "POST",
-      data: {
-      "fatto": "sì" },
-      success: function (data) {
-        console.log(data);
-       },
-      error: function (richiesta, stato, errori) {
-         alert("E' avvenuto un errore. " + errore); }
-      });
-
-     getdata(prompt('dammi il tuo nome'));
-});
-
-function getdata(nomeUtente){
-$.ajax({
-  url: "http://157.230.17.132:3031/todos",
-  method: "POST",
-  data: {
-  "nome": nomeUtente,
-  "fatto": "sì" },
-  success: function (data) {
-    console.log(data);
-   },
-  error: function (richiesta, stato, errori) {
-     alert("E' avvenuto un errore. " + errore); }
+function postData(nomeUtente){
+  $.ajax({
+    url: "http://157.230.17.132:3031/todos",
+    method: "POST",
+    data: {
+    "nome": nomeUtente,
+    "fatto": "sì" },
+    success: function (data) {
+      console.log(data);
+     },
+    error: function (richiesta, stato, errori) {
+       alert("E' avvenuto un errore. " + errore); }
   });
+};
+
+function deleteData(id){
+  $.ajax({
+    url: "http://157.230.17.132:3031/todos/" + id,
+    method: "DELETE",
+    success: function (data) {
+      console.log(data);
+     },
+    error: function (richiesta, stato, errori) {
+       alert("E' avvenuto un errore. " + errore); }
+    });
 };
