4e444edc059d16478b6ae14fda96c1ace4b0117f
Stefano Riccio <riccio.stefano0@gmail.com>
2020-02-13 16:37:57 +0100

    fatta funzione di printing

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 // alert('ciao');
 $(document).ready(function(){
-getData()
+printData()
 });
+
+// FUNCTIONS------------->
+
+// funzione per stampare i dati
+function printData (){
+  $.ajax({
+    url: "http://157.230.17.132:3031/todos",
+    method: "GET",
+    success: function (data) {
+      var source = $('#entry-template').html();
+      var template = Handlebars.compile(source);
+
+      for (var i = 0; i < data.length; i++) {
+        var toDo = data[i];
+        var context = {
+          text: toDo.text
+        };
+        var html = template(context);
+        $('.todolist').append(html);
+      }
+     },
+    error: function (richiesta, stato, errori) {
+       alert("E' avvenuto un errore. " + errore); }
+    });
+};
+
+// funzione per chiamare i dati
 function getData(){
   $.ajax({
     url: "http://157.230.17.132:3031/todos",
@@ -17,6 +44,7 @@ function getData(){
     });
 }
 
+// funzione per pushare i dati
 function postData(nomeUtente){
   $.ajax({
     url: "http://157.230.17.132:3031/todos",
@@ -31,12 +59,13 @@ function postData(nomeUtente){
   });
 };
 
+// funzione per elminiare i dati
 function deleteData(id){
   $.ajax({
     url: "http://157.230.17.132:3031/todos/" + id,
     method: "DELETE",
     success: function (data) {
-      
+
      },
     error: function (richiesta, stato, errori) {
        alert("E' avvenuto un errore. " + errore); }
